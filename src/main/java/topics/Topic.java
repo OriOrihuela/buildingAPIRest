f93e9dc39d6d970258f57406072d527851bfe711
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-05-05 17:04:57 +0200

    Topic class done.

--- src/main/java/topics/Topic.java
+++ src/main/java/topics/Topic.java
@@ -1,4 +1,48 @@
 package topics;
 
 public class Topic {
+
+    /* ---- Properties ---- */
+    private String id = null;
+    private String name = null;
+    private String description = null;
+
+
+    /* ---- Constructors ---- */
+    public Topic() {
+    }
+
+    public Topic(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+
+    /* ---- Getters ---- */
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    /* ---- Setters ---- */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
