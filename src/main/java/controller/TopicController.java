ced558bd3a8c5a7351dfb1a32efe67b5961fda5f
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-05-05 17:23:08 +0200

    getAllTopics() response implemented.

--- src/main/java/controller/TopicController.java
+++ src/main/java/controller/TopicController.java
@@ -1,7 +1,21 @@
 package controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import topics.Topic;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class TopicController {
+
+    /* ---- Requests ---- */
+    @RequestMapping(path = "/topics")
+    public List<Topic> getAllTopics() {
+        return Arrays.asList(
+                new Topic("spring", "Spring Framework", "Spring Framework Description"),
+                new Topic("java", "Core Java", "Core Java Description"),
+                new Topic("javascript", "JavaScript", "JavaScript Description"));
+    }
 }
